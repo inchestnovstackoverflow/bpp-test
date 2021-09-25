afb490a12287b778148b365f209ec6cc89217b2a
igorchestnov <chestnov@haulmont.com>
2021-09-25 20:37:45 +0400

    Initial commit

---
+++ src/test/java/com/github/bpptest/BppTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.bpptest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BppTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
